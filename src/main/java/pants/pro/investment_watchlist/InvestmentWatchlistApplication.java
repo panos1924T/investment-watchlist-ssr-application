1704056e851eb025f733ef53dad61c3908ae39ef
PanTs <pants.ath@gmail.com>
2026-04-06 17:25:13 +0300

    initial commit

---
+++ src/main/java/pants/pro/investment_watchlist/InvestmentWatchlistApplication.java
@@ -0,0 +1,13 @@
+package pants.pro.investment_watchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentWatchlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentWatchlistApplication.class, args);
+	}
+
+}
