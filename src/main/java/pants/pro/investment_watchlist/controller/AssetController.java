e51a7ffac1e012bbe4ec989cabf545de1688b7c3
PanTs <pants.ath@gmail.com>
2026-04-08 17:29:47 +0300

    feat: enhance AssetController with asset form and asset types list methods

--- src/main/java/pants/pro/investment_watchlist/controller/AssetController.java
+++ src/main/java/pants/pro/investment_watchlist/controller/AssetController.java
@@ -3,12 +3,29 @@ package pants.pro.investment_watchlist.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pants.pro.investment_watchlist.dto.AssetInsertDTO;
+import pants.pro.investment_watchlist.dto.AssetTypeReadOnlyDTO;
+
+import java.util.List;
 
 @Controller
 @RequestMapping ("/assets")
 public class AssetController {
 
     public String getAssetForm(Model model) {
-        return "";
+        model.addAttribute("assetInsertDTO", AssetInsertDTO.empty());
+
+        return "asset-insert";
+    }
+
+    public List<AssetTypeReadOnlyDTO> assetTypes() {
+        return List.of(
+                new AssetTypeReadOnlyDTO(1L, "Stock"),
+                new AssetTypeReadOnlyDTO(1L, "ETF"),
+                new AssetTypeReadOnlyDTO(1L, "Bond"),
+                new AssetTypeReadOnlyDTO(1L, "Commodity"),
+                new AssetTypeReadOnlyDTO(1L, "Crypto")
+        );
+
     }
 }
