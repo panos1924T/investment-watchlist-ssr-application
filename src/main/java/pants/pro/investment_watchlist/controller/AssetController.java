288dd0acc6ab19b5ce24b7bc144348be80c45c31
PanTs <pants.ath@gmail.com>
2026-04-08 17:49:02 +0300

    feat: add GET mapping for asset form and enhance asset types list with distinct IDs

--- src/main/java/pants/pro/investment_watchlist/controller/AssetController.java
+++ src/main/java/pants/pro/investment_watchlist/controller/AssetController.java
@@ -2,6 +2,8 @@ package pants.pro.investment_watchlist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pants.pro.investment_watchlist.dto.AssetInsertDTO;
 import pants.pro.investment_watchlist.dto.AssetTypeReadOnlyDTO;
@@ -12,19 +14,21 @@ import java.util.List;
 @RequestMapping ("/assets")
 public class AssetController {
 
+    @GetMapping("/insert")
     public String getAssetForm(Model model) {
         model.addAttribute("assetInsertDTO", AssetInsertDTO.empty());
 
         return "asset-insert";
     }
 
+    @ModelAttribute("assetTypeReadOnlyDTO")
     public List<AssetTypeReadOnlyDTO> assetTypes() {
         return List.of(
                 new AssetTypeReadOnlyDTO(1L, "Stock"),
-                new AssetTypeReadOnlyDTO(1L, "ETF"),
-                new AssetTypeReadOnlyDTO(1L, "Bond"),
-                new AssetTypeReadOnlyDTO(1L, "Commodity"),
-                new AssetTypeReadOnlyDTO(1L, "Crypto")
+                new AssetTypeReadOnlyDTO(2L, "ETF"),
+                new AssetTypeReadOnlyDTO(3L, "Bond"),
+                new AssetTypeReadOnlyDTO(4L, "Commodity"),
+                new AssetTypeReadOnlyDTO(5L, "Crypto")
         );
 
     }
