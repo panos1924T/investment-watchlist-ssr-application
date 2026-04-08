38722b8c4c23735dd8a97649756b28b317d3801a
PanTs <pants.ath@gmail.com>
2026-04-08 18:24:31 +0300

    feat: add POST mapping to handle asset insertion with validation and success redirection

--- src/main/java/pants/pro/investment_watchlist/controller/AssetController.java
+++ src/main/java/pants/pro/investment_watchlist/controller/AssetController.java
@@ -1,11 +1,16 @@
 package pants.pro.investment_watchlist.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pants.pro.investment_watchlist.dto.AssetInsertDTO;
+import pants.pro.investment_watchlist.dto.AssetReadOnlyDTO;
 import pants.pro.investment_watchlist.dto.AssetTypeReadOnlyDTO;
 
 import java.util.List;
@@ -21,6 +26,20 @@ public class AssetController {
         return "asset-insert";
     }
 
+    @PostMapping("/insert")
+    public String assetInsert(@Valid @ModelAttribute("assetInsertDTO")AssetInsertDTO assetInsertDTO,
+                              BindingResult bindingResult, Model model,
+                              RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            return "asset-insert";
+        }
+
+        AssetReadOnlyDTO assetReadOnlyDTO = new AssetReadOnlyDTO("abc123", "Apple", "APPL");
+        redirectAttributes.addFlashAttribute("assetReadOnlyDTO", assetReadOnlyDTO);
+        return "redirect:/assets/success";
+    }
+
     @ModelAttribute("assetTypeReadOnlyDTO")
     public List<AssetTypeReadOnlyDTO> assetTypes() {
         return List.of(
@@ -30,6 +49,10 @@ public class AssetController {
                 new AssetTypeReadOnlyDTO(4L, "Commodity"),
                 new AssetTypeReadOnlyDTO(5L, "Crypto")
         );
+    }
 
+    @GetMapping("/success")
+    public String assetSuccess(Model model) {
+        return "asset-success";
     }
 }
