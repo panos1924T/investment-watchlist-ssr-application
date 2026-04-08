1a47ef829c809f2585544a50cbed83394fc357e7
PanTs <pants.ath@gmail.com>
2026-04-08 17:15:50 +0300

    feat: add AssetInsertDTO with validation annotations for asset creation

---
+++ src/main/java/pants/pro/investment_watchlist/dto/AssetInsertDTO.java
@@ -0,0 +1,18 @@
+package pants.pro.investment_watchlist.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record AssetInsertDTO(
+        @NotNull(message = "Company name must not be null.")
+        @Size(min = 3, message = "Company name must contain at least three characters.")
+        String name,
+
+        @NotNull(message = "Ticker must not be null!")
+        @Size(min = 2, max = 10, message = "Ticker's length must be between two to ten characters.")
+        String ticker,
+
+        @NotNull(message = "Asset Type must not be null!")
+        Long assetTypeId
+) {
+}
