b26e0b80d6f6ec0e8a3264b23e1afdcd6b494a02
PanTs <pants.ath@gmail.com>
2026-04-08 22:38:12 +0300

    feat: add custom exceptions for invalid arguments and missing entities

---
+++ src/main/java/pants/pro/investment_watchlist/core/exceptions/EntityInvalidArgumentException.java
@@ -0,0 +1,8 @@
+package pants.pro.investment_watchlist.core.exceptions;
+
+public class EntityInvalidArgumentException extends Exception{
+
+    public EntityInvalidArgumentException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/pants/pro/investment_watchlist/core/exceptions/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package pants.pro.investment_watchlist.core.exceptions;
+
+public class EntityNotFoundException extends Exception {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
