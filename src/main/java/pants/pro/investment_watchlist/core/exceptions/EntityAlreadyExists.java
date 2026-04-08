af64241de49823837c839482f6afd25ba419f6fe
PanTs <pants.ath@gmail.com>
2026-04-08 22:40:53 +0300

    feat: add EntityAlreadyExists exception for handling duplicate entities

---
+++ src/main/java/pants/pro/investment_watchlist/core/exceptions/EntityAlreadyExists.java
@@ -0,0 +1,8 @@
+package pants.pro.investment_watchlist.core.exceptions;
+
+public class EntityAlreadyExists extends Exception {
+
+    public EntityAlreadyExists(String message) {
+        super(message);
+    }
+}
