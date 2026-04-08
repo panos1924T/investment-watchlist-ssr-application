fa8c9a60bdd3eb3f2fe74a2c0696bfa67a259e05
PanTs <pants.ath@gmail.com>
2026-04-08 22:38:41 +0300

    refactor: remove unused JsonTypeInfo import from AssetRepository

--- src/main/java/pants/pro/investment_watchlist/repository/AssetRepository.java
+++ src/main/java/pants/pro/investment_watchlist/repository/AssetRepository.java
@@ -1,6 +1,5 @@
 package pants.pro.investment_watchlist.repository;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pants.pro.investment_watchlist.model.Asset;
 
@@ -9,5 +8,4 @@ import java.util.Optional;
 public interface AssetRepository extends JpaRepository<Asset, Long> {
 
     Optional<Asset> findByTicker(String ticker);
-
 }
