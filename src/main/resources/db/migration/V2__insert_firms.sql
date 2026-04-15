9ae3fbbedc00460662fcd94ccbf77be95dbbddd3
PanTs <pants.ath@gmail.com>
2026-04-15 16:36:44 +0300

    refactor: reorder firm seed data in migration script for better readability

--- src/main/resources/db/migration/V2__insert_firms.sql
+++ src/main/resources/db/migration/V2__insert_firms.sql
@@ -1,12 +1,12 @@
 INSERT INTO firms (id, name) VALUES
-(1,'JPMorgan Chase'),
-(2,'Goldman Sachs'),
-(3,'Morgan Stanley'),
-(4,'Bank of America Merrill Lynch'),
-(5,'Citigroup'),
-(6,'Wells Fargo'),
-(7,'HSBC'),
-(8,'Deutsche Bank'),
-(9,'Credit Suisse'),
-(10,'Barclays');
+    (1,'Bank of America Merrill Lynch'),
+    (2,'Barclays'),
+    (3,'Citigroup'),
+    (4,'Credit Suisse'),
+    (5,'Deutsche Bank'),
+    (6,'Goldman Sachs'),
+    (7,'HSBC'),
+    (8,'JPMorgan Chase'),
+    (9,'Morgan Stanley'),
+    (10,'Wells Fargo');
 ALTER TABLE firms AUTO_INCREMENT = 11;
