a78d627e20aa545bddd1cc347a4de0574de528fa
PanTs <pants.ath@gmail.com>
2026-04-15 17:37:53 +0300

    feat: externalize validation messages and update AnalystInsertDTO annotations

--- src/main/java/pants/pro/investment_watchlist/dto/AnalystInsertDTO.java
+++ src/main/java/pants/pro/investment_watchlist/dto/AnalystInsertDTO.java
@@ -6,19 +6,19 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record AnalystInsertDTO(
-        @NotBlank(message = "First name must not be blank!")
-        @Size(min = 2, max = 50, message = "First name's length must be between 2 and 50 characters.")
+        @NotNull
+        @Size(min = 2, max = 50)
         String firstname,
 
-        @NotBlank(message = "Last name must not be blank!")
-        @Size(min = 2, max = 50, message = "Last name's length must be between 2 and 50 characters.")
+        @NotNull
+        @Size(min = 2, max = 50)
         String lastname,
 
-        @NotBlank(message = "Email must not be blank!")
-        @Email(message = "Email must be a valid email address!")
+        @NotNull
+        @Email
         String email,
 
-        @NotNull(message = "Firm must not be null!")
+        @NotNull
         Long firmId
 ) {
 
