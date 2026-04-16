3133ee9bd9a57982978ac41e7335741ed69e52db
PanTs <pants.ath@gmail.com>
2026-04-16 19:18:20 +0300

    feat: initialize role entity

---
+++ src/main/java/pants/pro/investment_watchlist/model/Role.java
@@ -0,0 +1,23 @@
+package pants.pro.investment_watchlist.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+
+}
