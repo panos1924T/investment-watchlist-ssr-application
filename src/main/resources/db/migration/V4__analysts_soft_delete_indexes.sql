b97b99e8b8e1e3dcb1f8e8a536fbd09c77a0cd84
PanTs <pants.ath@gmail.com>
2026-04-16 16:53:01 +0300

    chore: rename soft delete migration scripts and indexes

---
+++ src/main/resources/db/migration/V4__analysts_soft_delete_indexes.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_analysts_deleted ON analysts (deleted);
+CREATE INDEX idx_analysts_deleted_at ON analysts (deleted_at);
--- src/main/resources/db/migration/V4__teachers_soft_delete_indexes.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX idx_teachers_deleted ON teachers (deleted);
-CREATE INDEX idx_teachers_deleted_at ON teachers (deleted_at);
