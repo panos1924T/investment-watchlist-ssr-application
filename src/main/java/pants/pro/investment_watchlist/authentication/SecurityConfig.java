92b01a8aa4b6431f244596787d865139f402196b
PanTs <pants.ath@gmail.com>
2026-04-17 20:45:03 +0300

    feat: implement custom authentication success handler for redirecting users after login

--- src/main/java/pants/pro/investment_watchlist/authentication/SecurityConfig.java
+++ src/main/java/pants/pro/investment_watchlist/authentication/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                //.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/", "/index.html").permitAll()
                         .requestMatchers("/login").permitAll()
