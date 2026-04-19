b571b92415025cf39c3d586327c92cb1be090419
PanTs <pants.ath@gmail.com>
2026-04-19 20:30:25 +0300

    feat: update header to include logout functionality and configure authentication handlers in security config

--- src/main/java/pants/pro/investment_watchlist/authentication/SecurityConfig.java
+++ src/main/java/pants/pro/investment_watchlist/authentication/SecurityConfig.java
@@ -7,16 +7,20 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration              // create a bean from a method
 @RequiredArgsConstructor
 @EnableMethodSecurity       // to enable @PreAuthorize annotation
 @EnableWebSecurity          // filter security
 public class SecurityConfig {
+
+    private final AuthenticationSuccessHandler authSuccessHandler;
+    private final AuthenticationFailureHandler authFailureHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -40,8 +44,8 @@ public class SecurityConfig {
                 )
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
-//                        .successHandler(authSuccessHandler)
-//                        .failureHandler(authFailureHandler)
+                        .successHandler(authSuccessHandler)
+                        .failureHandler(authFailureHandler)
                 )
                 .logout(logout -> logout
                         .logoutSuccessUrl("/login?logout=")
