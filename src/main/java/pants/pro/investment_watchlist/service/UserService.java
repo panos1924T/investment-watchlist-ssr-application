8360396b1d019357a75bfcf328efba1620ed2da5
PanTs <pants.ath@gmail.com>
2026-04-20 16:27:46 +0300

    feat: update saveUser method to encode password and associate user with role

--- src/main/java/pants/pro/investment_watchlist/service/UserService.java
+++ src/main/java/pants/pro/investment_watchlist/service/UserService.java
@@ -7,10 +7,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pants.pro.investment_watchlist.core.exceptions.EntityAlreadyExistsException;
 import pants.pro.investment_watchlist.core.exceptions.EntityInvalidArgumentException;
-import pants.pro.investment_watchlist.core.exceptions.EntityNotFoundException;
 import pants.pro.investment_watchlist.dto.UserInsertDTO;
 import pants.pro.investment_watchlist.dto.UserReadOnlyDTO;
 import pants.pro.investment_watchlist.mapper.Mapper;
+import pants.pro.investment_watchlist.model.Role;
+import pants.pro.investment_watchlist.model.User;
 import pants.pro.investment_watchlist.repository.RoleRepository;
 import pants.pro.investment_watchlist.repository.UserRepository;
 
@@ -25,7 +26,7 @@ public class UserService implements IUserService {
     private final RoleRepository roleRepository;
 
     @Override
-    @Transactional(rollbackFor = {EntityNotFoundException.class, EntityInvalidArgumentException.class})
+    @Transactional(rollbackFor = {EntityAlreadyExistsException.class, EntityInvalidArgumentException.class})
     public UserReadOnlyDTO saveUser(UserInsertDTO userInsertDTO)
             throws EntityAlreadyExistsException, EntityInvalidArgumentException {
 
@@ -34,6 +35,14 @@ public class UserService implements IUserService {
             if (userRepository.findByUsername(userInsertDTO.username()).isPresent()) {
                 throw new EntityAlreadyExistsException("User with username=" + userInsertDTO.username() + " already exists!");
             }
+
+            User user = mapper.toUserEntityDTO(userInsertDTO);
+            user.setPassword(passwordEncoder.encode(userInsertDTO.password()));
+            Role role = roleRepository.findById(userInsertDTO.roleId())
+                    .orElseThrow(() -> new EntityInvalidArgumentException("Role id=" + userInsertDTO.roleId() + " invalid!"));
+            role.addUser(user);
+            log.info("Save succeeded for user with username={}.", userInsertDTO.username());
+            return mapper.toUserReadOnlyDTO(user);
 
         } catch (EntityAlreadyExistsException e) {
             log.error("Save failed! User with username={} already exists", userInsertDTO.username());
